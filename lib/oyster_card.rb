f02fe885a4f7f59360dee71487e6d48a4414595f
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-07 21:58:21 +0100

    Refactoring
    Refactoring

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,16 +1,18 @@
 class OysterCard
   attr_reader :balance
-  attr_accessor :top_up
-  def initialize(top_up)
+  def initialize
     @balance = 0
-    @top_up = top_up
   end
 
-DEFAULT_LIMIT = 90
+  def top_up(top_up)
+    @balance += top_up
+      error_message = "top up limit reached"
+      raise error_message if limit?
+    return "Your balance is now #{@balance}"
+  end
 
-  def top_up
-    error_message = 'top up limit reached'
-    raise error_message if @balance > DEFAULT_LIMIT
-    @balance += @top_up
+  DEFAULT_LIMIT = 90
+  def limit?
+    @balance > DEFAULT_LIMIT
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -2,17 +2,17 @@ require "oyster_card.rb"
 describe OysterCard do
 	describe '#initialize' do
 		it "default balance 0" do
-			oyster = OysterCard.new(0)
+			oyster = OysterCard.new
 			expect(oyster.balance).to eq(0)
 		end
 	describe '#.topup' do
 		it "adds given value to balance" do
-			oyster = OysterCard.new(25)
-			expect(oyster.top_up).to eq(25)
+			oyster = OysterCard.new
+			expect(oyster.top_up(25)).to include("25")
 		end
 		it 'raises error, max limit reached' do
-			oyster = OysterCard.new(91)
-			expect{ oyster.top_up }.to raise_error 'top up limit reached'
+			oyster = OysterCard.new
+			expect{ oyster.top_up(91) }.to raise_error 'top up limit reached'
 		end
 	end
   end
