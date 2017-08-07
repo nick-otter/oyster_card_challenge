4acfe9849f2cc4b24689d6c0f98a344b7dca5fb7
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-07 16:07:20 +0100

    Step 5, added top up functionality

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,9 +1,12 @@
 class OysterCard
-
-attr_reader :balance
-
-  def initialize
-   @balance = 0
+  attr_reader :balance
+  attr_accessor :top_up
+  def initialize(top_up)
+    @balance = 0
+    @top_up = top_up
   end
 
+  def new_balance
+    return @balance + @top_up
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,8 +1,15 @@
 require "oyster_card.rb"
 describe OysterCard do
-   describe "#initialize" do
-     it "default balance 0" do
-	  expect(subject.balance).to eq(0)
-end
+	describe '#initialize' do
+		it "default balance 0" do
+			oyster = OysterCard.new(0)
+			expect(oyster.balance).to eq(0)
+		end
+	describe '#.topup' do
+		it "adds given value to balance" do
+			oyster = OysterCard.new(25)
+			expect(oyster.new_balance).to eq(25)
+		end
+    end
 end
 end
